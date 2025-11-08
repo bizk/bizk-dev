633eb0248b514d75bcd2c5b96c879725b4dcc463
bizk <santiago.yanzon1999@gmail.com>
2025-11-08 12:14:37 -0300

    added latest work experience

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,31 @@ function App() {
       <h1>Carlos Santiago Yanzon</h1>
       <p>Informatics Engineer | Distributed Systems and blockchain Developer | Creative</p>
 
-      <h2>About me</h2>
+      <h2></h2>
 
 
-      <h2>Work Experience</h2>
+      
+      <h2>Work</h2>
+
+      <p>Carlos is a self driven professional and an Engineer with many years of experience in the industry working on complex porjects, at large scale companies and startups. He also has experience in entrepreneurship giving him the ability to communicate with different stakeholders. Carlos gets the job done and adapts to different circunstances with the tools and resources available (Some may say this is an Argentinian trait). He believes in Honest and direct communication, prefers dynamic work environments and is motivated by challenge.</p>
+
+
+      <h3>Blockchain and Backend Developer @ sFox</h3>
+      <p>2024 - Present</p>
+      <p>sFox is a crypto OTC exchange that processes millons of transactions daily. Due to my adaptability and deep blockchain knowledge, I have been working on different projects using different technologies, from putting together critical services to working on fullstack features.</p>
       <ul>
-        <li>
-          <h3>Distributed Systems Developer</h3>
-          <p>2020 - 2021</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-        </li>
+        <li>Created the UTXO wallet signing service on Go with HSM and PKCS#11 for Ledger devices. It handles over 10k daily transactions.</li>
+        <li>Led the development of the travel rule compliance system and data exchange for US, Europe and Asia. Allowing the company to operate in these regions.</li>
+        <li>Worked on integrating rosetta with the ETH wallet services and the exchange API.</li>
+        <li>Worked on a Polkadot staking service. Mainly on a feature that scans and cooses the best validator to stake on.</li>
+      </ul>
+
+      <h3>Blockchain Developer @ Zondax</h3>
+      <p>2024 - Present</p>
+      <p></p>
+      <ul>
+        <li>Worked on the development of the Zondax Wallet SDK for iOS and Android. It is a library that allows developers to integrate Zondax's wallet services into their apps.</li>
+        <li></li>
       </ul>
 
       <h2>Education</h2>
